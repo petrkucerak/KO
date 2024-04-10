b4efc83d99ac8722b568f4bbb821f439b7758cbc
petrkucerak <petr@khome.cz>
2024-04-10 16:07:57 +0200

    Add solution

--- contest/main.py
+++ contest/main.py
@@ -11,37 +11,49 @@ if len(sys.argv) < 3:
 path_input = sys.argv[1]
 path_output = sys.argv[2]
 
-def contains_cycle(node):
-    # for i in range(edge_count):
-    #     if graph[i][2] >5: return True
-    return False
-
 nodes_count = 0
-
-# Existuje li hrana, musi byt cas jejiho zavreni nizsi nez hrany zavrene po ni
 
 with open(path_input, "r") as f:
 
     edge_count = int(f.readline())
-    graph = np.zeros((edge_count, 3))
+    graph = []
     for i in range(edge_count):
         edge = f.readline().split()
-        graph[i] = [int(edge[0]), int(edge[1]), int(edge[2])]
-        if graph[i][0] > nodes_count: nodes_count = round(graph[i][0])
-        if graph[i][1] > nodes_count: nodes_count = round(graph[i][1])
+        graph.append({
+            "in": int(edge[0]) - 1,
+            "out": int(edge[1]) - 1,
+            "cost": int(edge[2])
+        })
+
+        if graph[i]["in"] > nodes_count:
+            nodes_count = round(graph[i]["in"])
+        if graph[i]["out"] > nodes_count:
+            nodes_count = round(graph[i]["out"])
+
+# print(graph, nodes_count)
+
+nodes_count += 1
 
 m = g.Model()
 
-e = m.addVars(edge_count, vtype=g.GRB.BINARY)
+e = m.addVars(edge_count, vtype=g.GRB.BINARY, name="Existence")
+t = m.addVars(nodes_count, vtype=g.GRB.INTEGER,
+              lb=0, ub=nodes_count, name="Time")
 
-m.setObjective(g.quicksum(e[i]*graph[i][2] for i in range(edge_count)), g.GRB.MAXIMIZE)
+for i in range(edge_count):
+    print(graph[i]["out"])
+    m.addConstr(e[i] * t[graph[i]["out"]] + 1 <= t[graph[i]["in"]])
+
+m.setObjective(g.quicksum(graph[i]["cost"]*e[i]
+               for i in range(edge_count)), g.GRB.MAXIMIZE)
 
 m.optimize()
 
 ret = ""
+objective = 0
 for i in range(edge_count):
     if round(e[i].X) == 0:
-        ret += str(round(graph[i][0])) + " " + str(round(graph[i][1])) + "\n"
-print(ret)
+        ret += str(graph[i]["in"] + 1) + " " + str(graph[i]["out"] + 1) + "\n"
+        objective += graph[i]["cost"]
 with open(path_output, "w+") as f:
-    f.write(ret)
+    f.write(str(objective) + "\n" + ret)
